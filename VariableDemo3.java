9b62f3d2d9e46eb2dadf67f44db658a4079f715b
邵庆玲 <shaoqingling@192.168.2.197>
2022-11-19 10:40:43 +0800

    commit

---
+++ VariableDemo3.java
@@ -0,0 +1,17 @@
+public class VariableDemo3 {
+    public static void manin(String[]args){
+        int a = 10;
+        System.out.println(a);
+
+        short b = 20;
+        System.out.println(b);
+
+        int c = 30;
+        System.out.println(c);
+
+        long d = 9999999999L;
+        System.out.println(d);
+
+    }
+    
+}
