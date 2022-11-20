9b468027320fcc6b6a3e0bcb311080cd4153725b
邵庆玲 <shaoqingling@192.168.2.197>
2022-11-20 18:31:44 +0800

    commit

---
+++ ArithmeticOperation.java
@@ -0,0 +1,41 @@
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        //整数加法
+        System.out.println( 3 +  2);
+
+        //整数减法
+        System.out.println(3 - 2 );
+
+        //整数乘法
+        System.out.println(3 * 2);
+
+        //整数除法
+        System.out.println(10 / 5);
+
+        //取模
+        System.out.println(10 % 3);
+
+        //小数加法
+        System.out.println(1.2 + 1.3);
+        System.out.println(1.2 + 1.13);
+
+        //小数减法
+        System.out.println(1.2 - 1);
+        System.out.println(1.2 - 0.1);
+
+        //小数乘法
+        System.out.println(1.2 * 2);
+        System.out.println(1.2 * 1.1);
+
+        //小数除法
+        System.out.println(3 / 1.5);
+        System.out.println(3.3 / 1.1);
+
+        //小数取模
+        System.out.println(12.6 % 2);
+
+
+
+
+    }
+}
